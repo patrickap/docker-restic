32f9ff7cf51f580a51681eeb67685fdb114e504c
patrickap <patrick.schlageter@web.de>
2023-10-16 20:42:30 +0200

    feat: improve scripts

--- scripts/_dump/prune.sh
+++ scripts/_dump/prune.sh
@@ -2,12 +2,21 @@
 
 set -o pipefail
 
-log -i "Pruning backup archives at '${RESTIC_EXPORT}' ..."
+log -i "Searching for backup archives to prune at '${RESTIC_EXPORT}' ..."
 log -i "Keeping last ${RESTIC_DUMP_KEEP_LAST} backup archives."
-ls -t ${RESTIC_EXPORT}/backup_* | tail +$((RESTIC_DUMP_KEEP_LAST+1)) | xargs -r rm
+prune_backups=$(ls -t ${RESTIC_EXPORT}/backup_* | tail +$((RESTIC_DUMP_KEEP_LAST+1)) | xargs -r echo)
 
-if [ $? -ne 0 ]; then
-  log -w "Could not prune backup archives."
+if [ -n "$prune_backups" ]; then
+  # TODO: fix only logging first list entry
+  # $(echo $prune_backups | xargs -n 1 basename | xargs echo)
+  log -i "Pruning backup archives: '$(basename "$prune_backups")' ..."
+  rm -rf ${prune_backups}
+  
+  if [ $? -ne 0 ]; then
+    log -w "Could not prune backup archives."
+  else
+    log -i "Pruned backup archives."
+  fi
 else
-  log -i "Pruned backup archives."
+  log -w "Nothing to prune. Prune policy may not be fulfilled."
 fi
