6646420495e82b7d5cfb400992964862c53732ca
patrickap <patrick.schlageter@web.de>
2023-10-17 15:15:04 +0200

    feat: list all archives pruned

--- scripts/_dump/prune.sh
+++ scripts/_dump/prune.sh
@@ -7,9 +7,7 @@ log -i "Keeping last ${RESTIC_DUMP_KEEP_LAST} backup archives."
 prune_backups=$(ls -t ${RESTIC_EXPORT}/backup_* | tail +$((RESTIC_DUMP_KEEP_LAST+1)) | xargs -r echo)
 
 if [ -n "$prune_backups" ]; then
-  # TODO: fix only logging first list entry
-  # $(echo $prune_backups | xargs -n 1 basename | xargs echo)
-  log -i "Pruning backup archives: '$(basename "$prune_backups")' ..."
+  log -i "Pruning backup archives: '$(echo $prune_backups | xargs basename | xargs -r echo)' ..."
   rm -rf ${prune_backups}
   
   if [ $? -ne 0 ]; then
