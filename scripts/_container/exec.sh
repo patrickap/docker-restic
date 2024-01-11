6e74a6b625d0f6ab09349c2ad881671912a1bc80
patrickap <patrick.schlageter@web.de>
2024-01-11 15:00:52 +0100

    fix: exec command

--- scripts/_container/exec.sh
+++ scripts/_container/exec.sh
@@ -2,19 +2,27 @@
 
 log -i "Searching for containers labeled '${RESTIC_CONTAINER_EXEC_LABEL}' ..."
 containers=$(docker ps -q --filter label="${RESTIC_CONTAINER_EXEC_LABEL}")
+error=0
 
 if [ -n "${containers}" ]; then
   log -i "Executing container commands ..."
-  for container in ${containers}; do log -i "Executing '${container}' ..."; done
-  command=$(docker inspect --format '{{index .Config.Labels "'${RESTIC_CONTAINER_EXEC_LABEL}'"}}' ${container})
-  docker exec ${container} /bin/sh -c "${command}" > /dev/null
+  for container in ${containers}; do 
+    command=$(docker inspect --format '{{index .Config.Labels "'${RESTIC_CONTAINER_EXEC_LABEL}'"}}' ${container})
 
-  if [ $? -ne 0 ]; then
-    log -e "Could not execute container commands."
-    exit 1
-  else
-    log -i "Executed container commands."
-  fi
+    log -i "Executing '${command}' in '${container}' ..."
+    docker exec ${container} /bin/sh -c "${command}" > /dev/null
+
+    if [ $? -ne 0 ]; then
+      log -e "Could not execute command."
+      error=1
+    else
+      log -i "Executed command."
+    fi
+  done
 else
   log -w "No containers found. Possibly nothing to execute."
+fi
+
+if [ ${error} == 1 ]; then
+  exit 1
 fi
