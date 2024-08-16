f99994657a01f0d85770f2b78e865e419a418e46
patrickap <patrick.schlageter@web.de>
2024-08-16 10:02:16 +0200

    fix: root vs restic user execution

--- docker-restic.sh
+++ docker-restic.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-exec su-exec restic just --justfile ${DOCKER_RESTIC_DIR}/config/restic.just --working-directory ${DOCKER_RESTIC_DIR} "${@}"
+
+if [ "$(id -u)" -eq 0 ]; then
+  exec su-exec restic just --justfile ${DOCKER_RESTIC_DIR}/config/restic.just --working-directory ${DOCKER_RESTIC_DIR} "${@}"
+else
+  just --justfile ${DOCKER_RESTIC_DIR}/config/restic.just --working-directory ${DOCKER_RESTIC_DIR} "${@}"
+fi
